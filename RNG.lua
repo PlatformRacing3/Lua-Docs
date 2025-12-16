baf8d53e29110de6e4f2fc2a69695a9171467d41
ThePizza1k <44148753+ThePizza1k@users.noreply.github.com>
2025-12-16 16:52:28 -0500

    Update RNG.lua

--- RNG.lua
+++ RNG.lua
@@ -2,16 +2,16 @@
 ...
 
 --- Functions the same as math.random(...), but using the RNG object's state.
---- @treturn number A random number based on the RNG object's state.
+--- @param ... 0, 1, or 2 number inputs determining the range of the pseudo-random output.
+--- @treturn number A pseudo-random number based on the RNG object's state.
 --- @usage RNG.random(12,24) -- returns an integer between 12 and 24, inclusive.
 ---- RNG.random(12) -- returns an integer between 1 and 12, inclusive.
 ---- RNG.random() -- returns a number in the range [0,1).
 function random(...)
 end
 
---- Remove the given AS3 listener from being called once the event fires.
---- If the listener is removed during the event's invocation, the listener's removal will be delayed until the end of the invocation.
---- @param as3_listener The AS3 listener to no longer call.
+--- Functions the same as math.randomseed(...), but affecting the RNG object's state.
+--- @param ... An optional number input to set the seed with. If none is given, a client-generated seed is used.
 --- @usage RNG.randomseed(151260123)
 function randomseed(...)
 end
