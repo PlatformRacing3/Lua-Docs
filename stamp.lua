c35e467fced781e68e1ad46cf5cdb96092d525d1
ThePizza1k <44148753+ThePizza1k@users.noreply.github.com>
2025-12-16 17:57:21 -0500

    Update stamp.lua

--- stamp.lua
+++ stamp.lua
@@ -7,6 +7,55 @@ width = 1
 --- The height, in pixels, of the stamp. By default 64. Read-only.
 height = 1
 
+--- Controls whether or not the bitmap is smoothed when scaled or rotated.
+--- <https://airsdk.dev/reference/actionscript/3.0/flash/display/Bitmap.html#Bitmap()>
+smoothing = false
+
+--- The x position, in pixels, of the stamp when it is the child of a sprite. By default 0.
+x = 0
+
+--- The y position, in pixels, of the stamp when it is the child of a sprite. By default 0.
+y = 0
+
+--- The horizontal scale of the stamp when it is the child of a sprite. By default 1.
+scaleX = 1.0
+
+--- The vertical scale of the stamp when it is the child of a sprite. By default 1.
+scaleY = 1.0
+
+--- The vertical scale of the stamp when it is the child of a sprite. By default 1.
+scaleY = 1.0
+
+--- The rotation of the stamp, in degrees, when it is the child of a sprite. By default 0.
+rotation = 0
+
+--- The alpha of the stamp, represented by a number in the range of 0 - 1. An alpha of 0 means the stamp is completely transparent; 1 means fully visible. By default 1.
+alpha = 1
+
+--- A number value that is multiplied with the red color channel when drawn. By default 1.
+redMultiplier = 1
+
+--- A number value that is multiplied with the green color channel when drawn. By default 1.
+greenMultiplier = 1
+
+--- A number value that is multiplied with the blue color channel when drawn. By default 1.
+blueMultiplier = 1
+
+--- Sets red, green, and blue multipliers based on a hexadecimal RGB value (e.g. 0xFF0000). Write-only.
+colorMultiplier = 0xFFFFFF
+
+--- A number value from -255 to 255 that is added to the alpha channel after it is multiplied by alpha. By default 0.
+alphaOffset = 0
+
+--- A number value from -255 to 255 that is added to the alpha channel after it is multiplied by redMultiplier. By default 0.
+redOffset = 0
+
+--- A number value from -255 to 255 that is added to the alpha channel after it is multiplied by greenMultiplier. By default 0.
+greenOffset = 0
+
+--- A number value from -255 to 255 that is added to the alpha channel after it is multiplied by blueMultiplier. By default 0.
+blueOffset = 0
+
 --- The blend mode of the stamp. By default "normal".
 --- Valid blend modes are defined here:
 --- <https://airsdk.dev/reference/actionscript/3.0/flash/display/BlendMode.html>
@@ -14,6 +63,16 @@ blendMode = "normal"
 
 --- Whether or not the stamp was destroyed. Calling `destroy` sets this to true. Read-only.
 destroyed = false
+
+--- Returns the bounds of the stamp as a Rectangle, relative to itself.
+--- <https://airsdk.dev/reference/actionscript/3.0/flash/geom/Rectangle.html>
+function getBounds()
+end
+
+--- Returns the bounds of the stamp as a Rectangle, relative to itself, excluding any strokes on shapes.
+--- <https://airsdk.dev/reference/actionscript/3.0/flash/geom/Rectangle.html>
+function getRect()
+end
 
 --- Renders a given stamp to the stamp using a given translation, scaling, and rotation.
 --- @tparam stamp stamp The sprite to render to the stamp.
@@ -36,6 +95,27 @@ end
 function drawSprite(sprite, x, y, scaleX, scaleY, rotation)
 end
 
+--- Renders a given text field to the stamp using a given translation, scaling, and rotation.
+--- @tparam textfield textfield The text field to render to the stamp.
+--- @tparam number x By how many pixels along the x-axis to move the text field when rendering to the stamp. By default 0.
+--- @tparam number y By how many pixels along the y-axis to move the text field when rendering to the stamp. By default 0.
+--- @tparam number scaleX By how much to scale the text field along the x-axis when rendering to the stamp. By default 1.
+--- @tparam number scaleY By how much to scale the text field along the y-axis when rendering to the stamp. By default 1.
+--- @tparam number rotation By how many degrees to rotate the text field when rendering to the stamp. By default 0.
+--- @see textfield
+function drawTextField(textfield, x, y, scaleX, scaleY, rotation)
+end
+
+--- Renders a given display object to the stamp using a given translation, scaling, and rotation. A display object can be a sprite, stamp, or text field.
+--- @tparam stamp/sprite/textfield The display object to render to the stamp.
+--- @tparam number x By how many pixels along the x-axis to move the display object when rendering to the stamp. By default 0.
+--- @tparam number y By how many pixels along the y-axis to move the display object when rendering to the stamp. By default 0.
+--- @tparam number scaleX By how much to scale the display object along the x-axis when rendering to the stamp. By default 1.
+--- @tparam number scaleY By how much to scale the display object along the y-axis when rendering to the stamp. By default 1.
+--- @tparam number rotation By how many degrees to rotate the display object when rendering to the stamp. By default 0.
+function drawDisplayObject(drawable, x, y, scaleX, scaleY, rotation)
+end
+
 --- Sets the ARGB value (alpha and color) of the pixel at the given coordinate.
 --- @tparam int x The x-coordinate of the pixel.
 --- @tparam int y The y-coordinate of the pixel.
@@ -52,6 +132,62 @@ end
 --- @tparam int color The ARGB value of the pixel. The simplest way to calculate this is with the 0xAARRGGBB hex format, where AA is the hex value from 0 - 255 for the alpha, RR for red, GG for green, and BB for blue.
 --- @usage someStamp.setPixel(10, 10, 0xFF00FF00) -- Sets the pixel at (10, 10) to fully opaque green.
 function setRect(x, y, width, height, color)
+end
+
+--- Sets the ARGB values (alpha and color) of the pixels within the given rectangle, based on a string input.
+--- @tparam string hexARGB A string containing ARGB data for each pixel in the hexadecimal format (e.g "FF808080FF000000" for the first pixel to be gray and the second pixel to be black)
+--- @tparam int x The x-coordinate of the top left corner of the rectangle. 0 by default.
+--- @tparam int y The y-coordinate of the top left corner of the rectangle. 0 by default.
+--- @tparam int width The width of the rectangle. Stamp width by default.
+--- @tparam int height The height of the rectangle. Stamp height by default.
+--- @usage local strBuff = {}
+---- local i = 1
+---- for x = 0, 63 do
+----     for y = 0, 63 do
+----         strBuff[i] = string.format("%.8x",0xFF000000 + math.random(0xFFFFFF)) -- Generate a random color for the pixel, and format it as a hex string.
+----         i = i + 1
+----     end
+---- end
+---- someStamp.setPixels(table.concat(strBuff),0,0,64,64) -- Set each pixel to a be a fully opaque random color
+function setPixels(hexARGB, x, y, width, height)
+end
+
+--- Fills the stamp with randomly generated noise
+--- @tparam int randomSeed The seed to use for random numbers.
+--- @tparam int low The lowest value to generate for each channel.
+--- @tparam int high The highest value to generate for each channel.
+--- @tparam int channelOptions An integer indication for which color channels to put noise in.
+--- @tparam boolean grayScale Whether the noise is grayscale or not. False by default.
+--- @usage someStamp.noise(512356,0x00,0xFF,7,false) -- Generates randomly colored noise
+function noise(randomSeed, low, high, channelOptions, grayScale)
+end
+
+--- Fills the stamp with randomly generated perlin noise.
+--- <https://airsdk.dev/reference/actionscript/3.0/flash/display/BitmapData.html#perlinNoise()>
+--- @tparam int baseX Frequency to use in the X direction.
+--- @tparam int baseY Frequency to use in the Y direction.
+--- @tparam int numOctaves Number of octaves to use for fractal noise. Higher numbers create greater detail, at the cost of more processing time.
+--- @tparam int randomSeed The seed to use for random numbers.
+--- @tparam boolean stitch If true, the method attempts to smooth the transition edges of the image to create seamless textures for tiling as a bitmap fill. Does not work with offsets.
+--- @tparam boolean fractalNoise If true, generates fractal noise, otherwise generates turbulence. An image with turbulence has visible discontinuities in the gradient that can make it better approximate sharper visual effects.
+--- @tparam int channelOptions An integer indication for which color channels to put noise in.
+--- @tparam boolean grayScale Whether the noise is grayscale or not. False by default.
+--- @param offsetsArray An AS3 array of numbers to use as the offsets, formatted as {x1, y1, x2, y2, ..., xn, yn}
+--- @usage someStamp.perlinNoise -- Generates randomly colored noise
+function perlinNoise(baseX, baseY, numOctaves, randomSeed, stitch, fractalNoise, channelOptions, grayScale, offsetsArray)
+end
+
+--- Scrolls the contents of the stamp.
+--- @tparam int x How much to scroll in the x direction.
+--- @tparam int y How much to scroll in the y direction.
+function scroll(x, y)
+end
+
+--- Performs a flood fill operation starting at an (x,y) coordinate, filling with a given color.
+--- @tparam int x How much to scroll in the x direction.
+--- @tparam int y How much to scroll in the y direction.
+--- @tparam int color The ARGB value for the color to fill with.
+function floodFill(x, y, color)
 end
 
 --- Creates a copy of the stamp with identical contents.
